f2e14881e98885abd240b1b06278868468645621
jonasrosenlind <rosenlindjonas@gmail.com>
2014-04-01 14:55:00 +0200

    woohoo

--- src/paketet/test.java
+++ src/paketet/test.java
@@ -3,6 +3,6 @@ package paketet;
 public class test {
 	public static void main (String[] args){
 		System.out.println("nu kan vi sammarbeta :) ");
-		System.out.println("you bet");
+		System.out.println("you bettar");
 	}
 }
