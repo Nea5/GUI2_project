c1777fb4230e528059d04d99c016ead6e2e7bf46
SunBurst <marcus.enderskog@gmail.com>
2014-04-01 14:51:17 +0200

    test branch

--- src/paketet/test.java
+++ src/paketet/test.java
@@ -4,5 +4,6 @@ public class test {
 	public static void main (String[] args){
 		System.out.println("nu kan vi sammarbeta :) ");
 		System.out.println("you bet");
+		System.out.println("testar branch");
 	}
 }
