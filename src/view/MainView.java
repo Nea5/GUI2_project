cd6fe0506353530e026513bf8c796a3438349d9f
Linnea Sandelin <linnea.sandelin@gmail.com>
2014-04-04 14:05:35 +0200

    MainView

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -1,5 +1,23 @@
 package view;
 
+import javax.swing.JFrame;
+
+import com.sun.codemodel.internal.JLabel;
+
 public class MainView {
 
+	JFrame mainFrame;
+	
+	
+	public MainView() {
+		mainFrame = new JFrame("Spirograph");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+	}
+	
+	public static void main(String[] args){
+		new MainView();
+	}
 }
