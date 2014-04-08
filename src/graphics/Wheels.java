628e86041154a468fed4683666c0b4fc00a57daf
Linnea Sandelin <linnea.sandelin@gmail.com>
2014-04-08 13:52:19 +0200

    Graphixxxxxxxxs

--- src/graphics/Wheels.java
+++ src/graphics/Wheels.java
@@ -1,14 +1,34 @@
 package graphics;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.GeneralPath;
 
 import javax.swing.JFrame;
 
-public class Wheels {
+import view.drawPanel;
+
+public class Wheels extends drawPanel {
 	
-	Graphics wheel1;
+	//public static Graphics wheel1;
+	//public static Shape wheel1;
+	public GeneralPath polyline;
 	
 	public Wheels() {
-		wheel1.drawRect(0, 0, 10, 10);
+		int x2Points[] = {0, 100, 0, 100};
+		int y2Points[] = {0, 50, 50, 0};
+		
+		polyline = new GeneralPath(GeneralPath.WIND_EVEN_ODD, x2Points.length);
+
+		polyline.moveTo(x2Points[0], y2Points[0]);
+		
+		for (int index = 1; index < x2Points.length; index++) {
+	         polyline.lineTo(x2Points[index], y2Points[index]);
+		};
+
+		//wheel1 = new GeneralPath();
+		
 	}
 }
--- src/view/drawPanel.java
+++ src/view/drawPanel.java
@@ -1,8 +1,12 @@
 package view;
 
+import graphics.Wheels;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Shape;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
@@ -18,8 +22,13 @@ public class drawPanel extends JPanel {
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawString("HEJ HOPP", 20, 20);
-		g.drawRect(10, 10, 5, 5);
+		//g.drawString("HEJ HOPP", 20, 20);
+		Graphics2D g2 = (Graphics2D) g;
+		//g2.draw(Wheels.wheel1);
+		g2.draw(Wheels.polyline);
+
+		//Graphics2D g2 = (Graphics2D) g;
+	    //g2.draw(Wheels.wheel1);
 	}
 	
 }
